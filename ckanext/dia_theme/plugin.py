86c681815909c00cdcf822ccc974463337f69c1e
William Hughes <williamh@catalyst.net.nz>
2017-02-07 10:15:57 +1300

    Add setting for parent site URL
    Makes life easier when running in a CKAN+CMS config
    Also fixes issue where admin links not added when logged in

--- ckanext/dia_theme/plugin.py
+++ ckanext/dia_theme/plugin.py
@@ -1,9 +1,21 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.common import config
+
+
+def parent_site_url():
+    """
+    Return the URL of the parent site (eg, if this instance
+    is running in a CKAN + CMS config). Returns the setting
+    ckan.parent_site_url, or value of h.url('home') if that
+    setting is missing
+    """
+    return config.get('ckan.parent_site_url', toolkit.h.url('home'))
 
 
 class Dia_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +23,8 @@ class Dia_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'dia_theme')
+
+    def get_helpers(self):
+        return {
+            'parent_site_url': parent_site_url
+        }

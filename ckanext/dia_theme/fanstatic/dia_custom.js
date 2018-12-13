895a81eb785e030a21f383b63a2e71db22798d13
matt-in-a-hat <matt@mediasuite.co.nz>
2018-12-13 13:48:35 +1300

    Add re-themed header & footer with rua pattern lib

--- ckanext/dia_theme/fanstatic/dia_custom.js
+++ ckanext/dia_theme/fanstatic/dia_custom.js
@@ -2,16 +2,15 @@
     $(document).ready(function() {
 
         // Site-wide search box
-        function updateSearchContext ($parent) {
-            var $form = $parent.find('form')
-            var dataOrContent = $parent.find('.search-form-context:checked').val()
+        function updateSearchContext ($form) {
+            var dataOrContent = $form.find('.search-form-context:checked').val()
             $form.attr('action', dataOrContent === 'datasets' ? '/dataset' : $('.ss-search-url').first().val())
             $form.find('input.search').attr('name', dataOrContent === 'datasets' ? 'q' : 'Search')
         }
 
         // Handle search form context switch
         $('.search-form-context').on('change', function (e) {
-            updateSearchContext($(this).closest('.site-search-wrap'))
+            updateSearchContext($(this).closest('form'))
         })
 
         // Initialise search form state
